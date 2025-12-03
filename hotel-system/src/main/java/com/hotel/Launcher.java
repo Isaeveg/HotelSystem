8c8cbc9907e5f7dfa18dd8cef59d113c1cea4545
Yehor <isaev.egor.08@gmail.com>
2025-12-03 17:02:20 +0100

    Initial version

---
+++ hotel-system/src/main/java/com/hotel/Launcher.java
@@ -0,0 +1,7 @@
+package com.hotel;
+
+public class Launcher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
---
+++ hotel-system/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.hotel {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.logging.log4j; 
+
+    opens com.hotel to javafx.fxml;
+    exports com.hotel;
+}
