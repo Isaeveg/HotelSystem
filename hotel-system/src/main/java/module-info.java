6d9277e912291c4e114cd9e95cacde646473acaa
valentin <nikiromanen@gmail.com>
2026-01-25 14:14:46 +0100

    AHAHAHA

--- hotel-system/src/main/java/module-info.java
+++ hotel-system/src/main/java/module-info.java
@@ -2,6 +2,7 @@ open module com.hotel {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
     requires java.sql;
     requires bcrypt;
 
